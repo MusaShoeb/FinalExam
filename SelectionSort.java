0afbd035248cc6d8247e1e23f6ce45b28155ce42
MusaShoeb <146212414+MusaShoeb@users.noreply.github.com>
2023-12-14 16:14:01 -0500

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,27 +1,24 @@
 package testingpackage;
 
+package testpackage;
+
 public class SelectionSort {
-		
-		public void selectionSort(int[] arr) {
-		if (arr == null || arr.length <= 1) {
-		return;
-		}
-		for (int i = 0; i < arr.length - 1; i++) {
-		int minIndex = i;
-		for (int j = i + 1; j < arr.length; j++) {
-		if (arr[j] < arr[minIndex]) {
-		minIndex = j;
-		}
-		}
-		if (minIndex <= i) {
-		swap(arr, i, minIndex);
-		}
-		}
-		}
-		private void swap(int[] arr, int i, int j) {
-			int temp = arr[i];
-		    arr[i] = arr[j];
-		    arr[j] = temp;
-		}
-		}
+private int temp;
+
+public SelectionSort() {
+}
+
+public int[] selectionSort(int[] x) {
+for (int i = 0; i < x.length; ++i) {
+for (int j= i+1; j < x.length; ++j) {
+if (x[i] > x[j]) {
+temp = x[i];
+x[i] = x[j];
+x[j] = temp;
+}
+} 
+} 
+return x;
+} 
+}
 
